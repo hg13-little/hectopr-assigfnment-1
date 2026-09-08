e5953c38827429183dd752d78c37d33e3784bd43
hg13-little <hector.gonzalez001@albright.edu>
2026-09-08 20:23:58 +0000

    comiitedd

--- city_data.py
+++ city_data.py
@@ -3,8 +3,8 @@
 # Starter Code from CSC 210
 # Modified by: [hector]
 class CityData:
-    
-       def __init__(self, name, start_year, data):
+
+    def __init__(self, name, start_year, data):
         self._name = name
         self._start_year = start_year
         self._data = data
@@ -14,10 +14,35 @@ class CityData:
     def name(self):
         return self._name
 
+    @property
+    def start_year(self):
+        return self._start_year
+
+    @property
+    def end_year(self):
+        return self._end_year
+
+    @property
+    def max_year(self):
+        biggest = max(self._data)
+        position = self._data.index(biggest)
+        return self._start_year + position
+
+    @property
+    def min_year(self):
+        smallest = min(self._data)
+        position = self._data.index(smallest)
+        return self._start_year + position
+
+    def population_growth(self, start_year, end_year):
+        return self[end_year] - self[start_year]
+
     def __getitem__(self, year):
         if not isinstance(year, int):
             raise TypeError("Year must be an integer")
+
         if year < self._start_year or year > self._end_year:
             raise IndexError("Year out of range")
-        return self._data[year - self._start_year]
-    
+
+        position = year - self._start_year
+        return self._data[position]
--- city_reader.py
+++ city_reader.py
@@ -1,12 +1,24 @@
 # city_reader.py
 # Read the city data from a CSV File
 # Starter Code from CSC 210
-# Modified by: [Your Name]
+# Modified by: [hector]
 
 from csv import DictReader
 from city_data import CityData
 
+
 def read_city_from_csv(file_path, city_name):
-    # YOUR CODE HERE
-    # Read the data for the specific city named 
-    # city_name into a CityData object and return it
+    years = []
+    populations = []
+    
+    with open(file_path, 'r') as file:
+        reader = DictReader(file)
+
+        for row in reader:
+            if row['City'] == city_name:
+                years.append(int(row['Year']))
+                populations.append(int(row['Population']))
+
+    start_year = years[0]
+
+    return CityData(city_name, start_year, populations)
